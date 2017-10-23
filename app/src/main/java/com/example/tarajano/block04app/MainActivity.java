49d4e4a53e0d4d080ed82a260d0057a50af51cc1
Manuel A. Alonso Tarajano <tarajano@gmail.com>
2017-10-22 22:44:31 -0400

    SUMMARY: Initial commit. Adds Play and Pause buttons and calabria.mp3 to raw/. REMARKS: coursera.org/learn/android-app
    FILE: raw/
      ADDS: calabria.mp3
      MODIFIES:
    
    FILE: activity_main
      ADDS: Play and pause buttons.
      MODIFIES:
      REMOVES:
    
    FILE: MainActivity
      ADDS: Corresponding actions/methods() for Play and Pause buttons.
      ADDS: MediaPlayer object to handle the actions from Play/Pause.

---
+++ app/src/main/java/com/example/tarajano/block04app/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.tarajano.block04app;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer song;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        song = MediaPlayer.create(this, R.raw.calabria);
+    }
+
+    public void playMusic(View v){
+        song.start();
+    }
+
+    public void pauseMusic(View v){
+        if(song.isPlaying()){
+            song.pause();
+        }
+    }
+}
